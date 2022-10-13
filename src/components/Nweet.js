3b486ec03c6a6cc99f9288f8cc28947659ffc9f5
woonan2 <lwhoo2273@naver.com>
2022-10-13 22:03:56 +0900

    delete and edit

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { db } from 'fbase';
 
 const Nweet = ({nweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+    const nweetTextRef = doc(db, "nwitter", `${nweetObj.id}`);
+
+    const onClickDelete = async () => {
+        const ok = window.confirm("Are you sure you want to delete this nweet?");
+        if (ok) {
+            await deleteDoc(nweetTextRef);
+        }
+    };
+
+    const toggleEditing = () => {
+        setEditing((prev) => !prev);
+    };
+
+    const onSubmit = async (e)=> {
+        e.preventDefault();
+        await updateDoc(nweetTextRef, {
+            text: newNweet,
+        });
+        setEditing(false);
+    };
+
+    const onChange = (e) => {
+        const {
+            target : { value },
+        } = e;
+        setNewNweet(value);
+    };
+
     return (
         <div>
-            <h4>{nweetObj.text}</h4>
-            { isOwner && (
-                <>
-                    <button>Delete Nweet</button>
-                    <button>Edit Nweet</button>
+            { editing ?
+                <> 
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="text"
+                            value={newNweet} 
+                            required 
+                            onChange={onChange}
+                        />
+                        <input type="submit" value="Update Nweet!" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
                 </>
-            )
+                : 
+                <>
+                <h4>{nweetObj.text}</h4>
+                {
+                    isOwner && (
+                        <>
+                            <button onClick={onClickDelete}>Delete Nweet</button>
+                            <button onClick={toggleEditing}>Edit Nweet</button>
+                        </>
+                    )
+                }
+                </>
             }
+            
         </div>
     )
 }
